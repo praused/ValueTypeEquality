80846f7c01e7ed4106888b592909815ab9db49b1
David Prause <praused@hotmail.com>
2017-05-17 11:47:35 -0400

    Value Type Equality Implemented with FoodItem struct

--- ValueTypeEquality/Program.cs
+++ ValueTypeEquality/Program.cs
@@ -10,6 +10,13 @@ namespace ValueTypeEquality
     {
         static void Main(string[] args)
         {
+            FoodItem banana = new FoodItem("banana", FoodGroup.Fruit);
+            FoodItem banana2 = new FoodItem("banana", FoodGroup.Fruit);
+            FoodItem chocolate = new FoodItem("chocolate", FoodGroup.Sweets);
+
+            Console.WriteLine("banana    ==  banana2:    " + (banana == banana2));
+            Console.WriteLine("banana2    ==  chocolate:    " + (banana2 == chocolate));
+            Console.WriteLine("chocolate    ==  banana:    " + (chocolate == banana));
         }
     }
 }
