e8abd96c1755cadd021aaa80d7310707f0fedb0f
David Prause <praused@hotmail.com>
2017-05-24 10:37:09 -0400

    mapping strings for hash codes

--- ValueTypeEquality/FoodItemEqualityComparer.cs
+++ ValueTypeEquality/FoodItemEqualityComparer.cs
@@ -16,12 +16,14 @@ namespace ValueTypeEquality
         
         public override bool Equals(FoodItem x, FoodItem y)
         {
-            return x.Name.ToUpperInvariant() == y.Name.ToUpperInvariant() && x.Group == y.Group;
+            //return x.Name.ToUpperInvariant() == y.Name.ToUpperInvariant() && x.Group == y.Group;
+            return StringComparer.OrdinalIgnoreCase.Equals(x.Name, y.Name) && x.Group == y.Group; //Can use System.StringComparer for strings
         }
 
         public override int GetHashCode(FoodItem obj)
         {
-            return obj.Name.ToUpperInvariant().GetHashCode() ^ obj.Group.GetHashCode();
+            //return obj.Name.ToUpperInvariant().GetHashCode() ^ obj.Group.GetHashCode();
+            return StringComparer.OrdinalIgnoreCase.GetHashCode(obj.Name) ^ obj.Group.GetHashCode(); //Can use System.StringComparer for strings
         }
     }
 }
